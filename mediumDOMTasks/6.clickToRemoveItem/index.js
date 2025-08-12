01364ce9b01f9c5eaca756ba5a1ac0ca0efb9389
Khishigdari <nbhishgee22@gmail.com>
2025-08-12 19:06:32 +0800

    task 6

--- mediumDOMTasks/6.clickToRemoveItem/index.js
+++ mediumDOMTasks/6.clickToRemoveItem/index.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 const body = document.querySelector("body");
 const p = document.createElement("p");
 const input = document.createElement("input");
@@ -7,12 +9,20 @@ const ul = document.createElement("ul");
 p.innerText = "Click to Remove Item";
 button.innerText = "Add";
 
-button.addEventListener("click", () => {
+let data = [];
+
+const add = (value) => {
+  data.push(value);
+};
+
+const createElement = (value) => {
   const li = document.createElement("li");
-  li.innerText = input.value;
   ul.appendChild(li);
-  input.value = "";
-});
+  button.addEventListener("click", () => {
+    li.innerText = input.value;
+    input.value = "";
+  });
+};
 
 function myFunction() {
   li.addEventListener("click", () => {
